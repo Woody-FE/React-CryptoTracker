4e679a5a81c839c200d9f02e86eaf7521dbe18bc
1millionTaein <kti91@likelion.org>
2022-02-01 21:37:23 +0900

    feat : 스타일 및 패치전 작업
    데이터 패치 전 배열값으로 확인 및 기본 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 import Router from "./Router";
+import { reset } from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&display=swap');
+  ${reset}
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color: ${({ theme }) => theme.bgColor};
+    color: ${({ theme }) => theme.textColor};
+  }
+`;
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,5 +1,86 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "Bitcoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "usdt-tether",
+    name: "Tether",
+    symbol: "USDT",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 function Coins() {
-  return <h1>Coins</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>Coins</Title>;
+      </Header>
+      <CoinsList>
+        {coins.map((coin) => (
+          <Coin key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+          </Coin>
+        ))}
+      </CoinsList>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  padding: 0 20px;
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: #fff;
+  color: ${({ theme }) => theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    display: block;
+    padding: 20px;
+    transition: color 0.2 ease-in;
+  }
+  &:hover {
+    a {
+      color: ${({ theme }) => theme.accentColor};
+    }
+  }
+`;
+
+const Title = styled.h1`
+  color: ${({ theme }) => theme.accentColor};
+  font-size: 48px;
+`;
 
 export default Coins;
